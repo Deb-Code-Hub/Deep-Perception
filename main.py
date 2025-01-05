e9c4e6ed32b5bf1eb07593e53ad008cd5069eb09
Deb-Code-Hub <debraj123mondal@gmail.com>
2025-01-05 14:24:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import struct
+import sys
+import tensorflow as tf
+import face_recognition
+from PIL import Image, ImageDraw
+import tkinter
+import pandas
+import dlib
+import matplotlib
+import keras
+import sklearn
+import re
+import base64
+print(tf.__version__)
+version=struct.calcsize("P")*8
+print(version)
+print(sys.version)
